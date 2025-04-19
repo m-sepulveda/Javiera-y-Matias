54aebe16eff5e31f6568c917795009d68129595b
m-sepulveda <mesm1998@gmail.com>
2025-04-18 22:21:53 -0400

    Se agregan mas cosas

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    data = {
-        'titulo': '♥ Matias y Javiera ♥'
-    }
-    target_date = datetime(2025, 7, 5, 15, 00, 0)
-    timestamp_ms = int(target_date.timestamp() * 1000)
-    return render_template('index.html',data=data,target_date=timestamp_ms)
+    @app.route('/')
+    def index():
+        data = {
+            'titulo': '♥ Matias y Javiera ♥'
+        }
+        target_date = datetime(2025, 7, 5, 15, 00, 0)
+        timestamp_ms = int(target_date.timestamp() * 1000)
+        return render_template('index.html',data=data,target_date=timestamp_ms)
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug =True, port=5000)
+    app = create_app()
+    app.run(debug =True, port=5000)``
