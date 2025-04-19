25e2228ae757e74e04dabb3fb7ea25b252a85fd2
m-sepulveda <mesm1998@gmail.com>
2025-04-18 22:32:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run(debug =True, port=5000)``
+    app.run(debug =True, port=5000)
